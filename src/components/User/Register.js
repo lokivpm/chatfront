c6a6a6ab98839a75167f68b70b6524d00a9f72c0
Loga priya Eswaran <logapriya202@gmail.com>
2024-11-04 11:37:45 +0530

    removed prevent e

--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -64,23 +64,35 @@ function Register() {
     }
   };
   
+
+
+
+  
   const handleLogin = async (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     setLoading(true);
+   
     try {
       const response = await axios.post(
         `${process.env.REACT_APP_FAST_API}/login/`,
         { email, password },
-        { withCredentials: true } 
+        { withCredentials: true }
       );
       setMessage(response.data.message);
       clearFormFields();
-      // navigate('/');
-      // window.location.reload();
+      navigate('/');
+      window.location.reload();
     } catch (error) {
-      setMessage('Login failed. Please try again.');
-    } finally {
-      setLoading(false);
+      if (error.response) {
+        console.error("Error response:", error.response.status, error.response.data);
+        setMessage(error.response.data.error || "An error occurred during login.");
+      } else if (error.request) {
+        console.error("Error request:", error.request);
+        setMessage("No response received from server.");
+      } else {
+        console.error("Error message:", error.message);
+        setMessage("An unknown error occurred.");
+      }
     }
   };
 
